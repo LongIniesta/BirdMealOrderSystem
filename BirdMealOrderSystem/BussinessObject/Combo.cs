c171794c143746fdaef97284110d9b175b7a9e2e
Bao Long <baolongtp54@gmail.com>
2023-07-05 09:42:57 +0700

    Staff

--- BirdMealOrderSystem/BussinessObject/Combo.cs
+++ BirdMealOrderSystem/BussinessObject/Combo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -13,13 +14,21 @@ namespace BussinessObject
             OrderComboDetails = new HashSet<OrderComboDetail>();
         }
 
+
         public int ComboId { get; set; }
+        [Required(ErrorMessage = "Combo name is required!")]
+        [StringLength(100, MinimumLength = 1, ErrorMessage = "The length of combo name is from 1 to 100 charater")]
         public string ComboName { get; set; }
+
+        [Required(ErrorMessage = "Price is required!")]
         public decimal Price { get; set; }
         public int Ratingavg { get; set; }
         [DiscountValid]
         public double Discount { get; set; }
+        [Required(ErrorMessage = "Combo description is required!")]
+        [StringLength(100, MinimumLength = 1, ErrorMessage = "The length of description is from 1 to 100 charater")]
         public string ComboDescription { get; set; }
+
         public bool ComboStatus { get; set; }
 
         public virtual ICollection<ComboDetail> ComboDetails { get; set; }
